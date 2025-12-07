6ed8f9effdcb86e97ce4b6f460d848b7532d732b
arzeee <146150750+arzeee@users.noreply.github.com>
2025-12-07 14:47:02 +0700

    Update run.bash

--- synchronization/time_sync/node_c/run.bash
+++ synchronization/time_sync/node_c/run.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
+# File: run.bash (Di Node C)
 
-python3 logger.py --bind 0.0.0.0 --port 9999 --expect 10
+echo "=== Menjalankan Node C ==="
+
+python3 peer_node.py \
+  --name C \
+  --listen 0.0.0.0 5000 \
+  --peers B@192.168.0.12:5000 D@192.168.0.14:5000 A@192.168.0.11:5000 \
+  --logger 192.168.0.10 9999
