fff10bad7a44f54b907062bfb00a55c061a2faa7
arzeee <146150750+arzeee@users.noreply.github.com>
2025-12-07 14:46:22 +0700

    Update run.bash

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+# File: run.bash (Di Node A)
+
+echo "=== Menjalankan Node A (Initiator) ==="
 
 python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
-	  --offset-ms 600 \
-	  --initiate-broadcast --msg "Hello from A"
-
+  --name A \
+  --listen 0.0.0.0 5000 \
+  --peers B@192.168.0.12:5000 C@192.168.0.13:5000 D@192.168.0.14:5000 \
+  --logger 192.168.0.10 9999 \
+  --initiate-broadcast \
+  --msg "Halo, simulasi dimulai dari A!"
