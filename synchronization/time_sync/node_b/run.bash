7775559c2cacde26ab1a47a274014e0d513768d9
arzeee <146150750+arzeee@users.noreply.github.com>
2025-12-07 14:46:43 +0700

    Update run.bash

--- synchronization/time_sync/node_b/run.bash
+++ synchronization/time_sync/node_b/run.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
+# File: run.bash (Di Node B)
+
+echo "=== Menjalankan Node B ==="
 
 python3 peer_node.py \
-	  --name B --listen 0.0.0.0 5001 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
-	  --offset-ms -600 
-
+  --name B \
+  --listen 0.0.0.0 5000 \
+  --peers A@192.168.0.11:5000 C@192.168.0.13:5000 \
+  --logger 192.168.0.10 9999
